6194a03f7c918ecda7f390fe0e90e583aa7c285f
Michal-Sitarz <me@michalsitarz.com>
2018-12-30 14:17:19 +0000

    tidy-up

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,2 +1,21 @@
+// event listeners
+document.getElementById("btnHelpdeskGuide").addEventListener("click",loadPage_HelpdeskGuide);
+document.getElementById("btnReportIncident").addEventListener("click",loadPage_ReportIncident);
+document.getElementById("btnNewRequest").addEventListener("click",loadPage_NewRequest);
+document.getElementById("btnTicketsHistory").addEventListener("click",loadPage_TicketsHistory);
 
+function loadPage_HelpdeskGuide(){
+    alert(" + - -- --- ----- ------- ----- --- -- - +\n\+  Helpdesk Guide available soon.  +\n\ + - -- --- ----- ------- ----- --- -- - +");
+}
 
+function loadPage_ReportIncident(){
+
+}
+
+function loadPage_NewRequest(){
+
+}
+
+function loadPage_TicketsHistory(){
+    window.location = "historical-tickets.html";
+}
