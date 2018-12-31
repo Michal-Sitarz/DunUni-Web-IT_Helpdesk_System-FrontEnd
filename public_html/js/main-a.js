6e0b75bb024d335b75d89678f111cc3ea45b5850
Michal-Sitarz <me@michalsitarz.com>
2018-12-31 15:09:46 +0000

    search bars - TDD

--- public_html/js/main-a.js
+++ public_html/js/main-a.js
@@ -1,9 +1,22 @@
+var inputSearchBarTICKET = document.getElementById("search-bar-ticket");
+var inputSearchBarUSER = document.getElementById("search-bar-user");
+var inputSearchBarDEVICE = document.getElementById("search-bar-device");
+
+setInputSearchBarToDefault(inputSearchBarTICKET, "Search for... TICKET");
+setInputSearchBarToDefault(inputSearchBarUSER, "Search for... USER");
+setInputSearchBarToDefault(inputSearchBarDEVICE, "Search for... DEVICE");
+
 // event listeners
 document.getElementById("btn-helpdesk-guide").addEventListener("click",loadPage_HelpdeskGuide);
 document.getElementById("btn-analytics").addEventListener("click",loadPage_Analytics);
 document.getElementById("btn-manage-users").addEventListener("click",loadPage_ManageUsers);
 document.getElementById("btn-manage-devices").addEventListener("click",loadPage_ManageDevices);
 
+inputSearchBarTICKET.addEventListener("focus",enterInputSearchBarTICKET);
+inputSearchBarTICKET.addEventListener("focusout",checkInputSearchBarTICKET);
+
+
+// load page functions
 function loadPage_HelpdeskGuide(){
     window.location = "helpdesk-guide.html";
 }
@@ -18,4 +31,40 @@ function loadPage_ManageUsers(){
 
 function loadPage_ManageDevices(){
     window.location = "manage-devices.html";
+}
+
+// set default appearance functions
+function setInputSearchBarToDefault(element, defaultText){
+    element.value = defaultText;
+    element.style.color = "gray";
+}
+/*
+function setInputSearchBarTICKET(){
+    inputSearchBarTICKET.value = "Search for... TICKET";
+    inputSearchBarTICKET.style.color = "gray";
+}
+
+function setInputSearchBarUSER(){
+    inputSearchBarUSER.value = "Search for... USER";
+    inputSearchBarUSER.style.color = "gray";
+}
+
+function setInputSearchBarDEVICE(){
+    inputSearchBarDEVICE.value = "Search for... DEVICE";
+    inputSearchBarDEVICE.style.color = "gray";
+}
+*/
+
+function enterInputSearchBarTICKET(){
+    if(inputSearchBarTICKET.value === "Search for... TICKET"){
+        inputSearchBarTICKET.value = "";
+        inputSearchBarTICKET.style.color = "black";
+    }
+}
+
+function checkInputSearchBarTICKET(){
+    if(inputSearchBarTICKET.value === "")
+    {
+        setInputSearchBarToDefault(inputSearchBarTICKET, "Search for... TICKET");
+    }
 }
