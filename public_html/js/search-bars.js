adadb60dbfa23b6976a0a37f8ad97b039e616bed
Michal-Sitarz <me@michalsitarz.com>
2019-01-04 11:41:53 +0000

    search bars functionality implemented

--- public_html/js/search-bars.js
+++ public_html/js/search-bars.js
@@ -2,21 +2,31 @@ var inputSearchBarTICKET = document.getElementById("search-bar-ticket");
 var inputSearchBarUSER = document.getElementById("search-bar-user");
 var inputSearchBarDEVICE = document.getElementById("search-bar-device");
 
+
 // search bar event handlers
 if(inputSearchBarTICKET != null){
     inputSearchBarTICKET.addEventListener("focus",enterInputSearchBarTICKET);
     inputSearchBarTICKET.addEventListener("focusout",checkInputSearchBarTICKET);
     setInputSearchBarToDefault(inputSearchBarTICKET, "Search for... TICKET");
+    
+    let inputSearchButtonTICKET = document.getElementById("search-bar-ticket").nextElementSibling;
+    inputSearchButtonTICKET.addEventListener("click",searchForTICKET);
 }
 if(inputSearchBarUSER != null){
     inputSearchBarUSER.addEventListener("focus",enterInputSearchBarUSER);
     inputSearchBarUSER.addEventListener("focusout",checkInputSearchBarUSER);
-    setInputSearchBarToDefault(inputSearchBarUSER, "Search for... USER"); 
+    setInputSearchBarToDefault(inputSearchBarUSER, "Search for... USER");
+    
+    let inputSearchButtonUSER = document.getElementById("search-bar-user").nextElementSibling;
+    inputSearchButtonUSER.addEventListener("click",searchForUSER);
 }
 if(inputSearchBarDEVICE != null){
     inputSearchBarDEVICE.addEventListener("focus",enterInputSearchBarDEVICE);
     inputSearchBarDEVICE.addEventListener("focusout",checkInputSearchBarDEVICE);
     setInputSearchBarToDefault(inputSearchBarDEVICE, "Search for... DEVICE");
+    
+    let inputSearchButtonDEVICE = document.getElementById("search-bar-device").nextElementSibling;
+    inputSearchButtonDEVICE.addEventListener("click",searchForDEVICE);
 }
 
 
@@ -70,5 +80,34 @@ function checkInputSearchBarDEVICE(){
     if(inputSearchBarDEVICE.value === "")
     {
         setInputSearchBarToDefault(inputSearchBarDEVICE, "Search for... DEVICE");
+    }
+}
+
+function validateSearchInput(input){
+    if(input.length <= 0 || input.includes("Search for...")){
+        alert("Please enter text to search.");
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+
+function searchForTICKET(){
+    if(validateSearchInput(inputSearchBarTICKET.value) === true){
+        //perform search query
+        alert("Entry not found");
+    }
+}
+function searchForUSER(){
+    if(validateSearchInput(inputSearchBarUSER.value) === true){
+        //perform search query
+        alert("Entry not found");
+    }
+}
+function searchForDEVICE(){
+    if(validateSearchInput(inputSearchBarDEVICE.value) === true){
+        //perform search query
+        alert("Entry not found");
     }
 }
