8d9c1d25839ad8729d93915546d5df1cb8fa7379
Michal-Sitarz <me@michalsitarz.com>
2019-01-11 12:43:29 +0000

    manage users: 'display details example' + functionality: edit, delete, reset password

--- public_html/js/new-add.js
+++ public_html/js/new-add.js
@@ -3,9 +3,10 @@ var addNewUser = document.getElementById("btn-add-new-user-submit");
 var addNewDevice = document.getElementById("btn-add-new-device-submit");
 var allInputFields = document.getElementsByClassName("input-add-new");
 
+// EVENT listeners
 if(addNewUser_CreateAccountItself != null){
     addNewUser_CreateAccountItself.addEventListener("click",submitNewUser);
-    document.getElementById("btn-create-account-cancel").addEventListener("click",cancelCreatingAccount);
+    document.getElementById("btn-create-account-cancel").addEventListener("click",loadpage_Login);
 }
 
 if(addNewUser != null){
@@ -29,9 +30,51 @@ if(document.getElementById("btn-device-remove") != null){
     });
 }
 
+//document.getElementById("btn-user-account-reset-password").addEventListener();
+if(document.getElementById("btn-user-account-update") != null){
+    document.getElementById("btn-user-account-reset-password").addEventListener("click",resetUserPassword);
+    document.getElementById("btn-user-account-update").addEventListener("click",updateUserAccount);
+    document.getElementById("btn-user-account-cancel").addEventListener("click",loadPage_ManageUsers);
+    document.getElementById("btn-user-account-delete").addEventListener("click",deleteUserAccount);
+}
 
-function cancelCreatingAccount(){
+function loadPage_ManageUsers(){
+    window.location = "manage-users.html";
+}
+
+function loadpage_Login(){
     window.location = "login.html";
+}
+
+function validateInputFields(){
+    for(let i=0; i<allInputFields.length; i++){
+        if(allInputFields[i].value == ""){
+            return false;
+        }
+    }
+    return true;
+}
+
+function clearInputFields(){
+    for(let i=0; i<allInputFields.length; i++){
+        allInputFields[i].value = "";
+    }
+}
+
+function submitNewDevice(){
+    if(validateInputFields() === true){
+        alert("New Device: "
+                +document.getElementById("add-new-device-hostname").value
+                +" ("
+                +document.getElementById("add-new-device-type").value
+                +") has been added to the database.");
+        
+        //assign values to Device object and update a database
+        clearInputFields();
+    }
+    else{
+        alert("Please fill in all the fields (all fields are mandatory!).");
+    }
 }
 
 function submitNewUser(){
@@ -53,34 +96,23 @@ function submitNewUser(){
     }
 }
 
-function submitNewDevice(){
-    if(validateInputFields() === true){
-        alert("New Device: "
-                +document.getElementById("add-new-device-hostname").value
-                +" ("
-                +document.getElementById("add-new-device-type").value
-                +") has been added to the database.");
-        
-        //assign values to Device object and update a database
+function updateUserAccount(){
+    if(validateInputFields()){
+        alert("User Account has been updated.");
         clearInputFields();
+        loadPage_ManageUsers();
     }
     else{
         alert("Please fill in all the fields (all fields are mandatory!).");
     }
 }
 
-function validateInputFields(){
-    for(let i=0; i<allInputFields.length; i++){
-        if(allInputFields[i].value == ""){
-            return false;
-        }
-    }
-    return true;
+function deleteUserAccount(){
+    alert("User Account has been deleted!");
+    clearInputFields();
+    loadPage_ManageUsers();
 }
 
-function clearInputFields(){
-    for(let i=0; i<allInputFields.length; i++){
-        allInputFields[i].value = "";
-    }
+function resetUserPassword(){
+    alert("User's Password has been reset to default:\n\nPassword-01\n\nUser should change it during first sign in.");
 }
-
